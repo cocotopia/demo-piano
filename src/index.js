0ef443475891792a086448d8cecb9bfd201f77a6
Cristhian Buiza <cbuiza@ucvvirtual.edu.pe>
2022-07-14 12:54:27 -0500

    deploy

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./login/LoginPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/principal" element={<App />} />
+        <Route path="/principal/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
